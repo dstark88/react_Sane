7f7019ae647ee72394b08349e816ed65e5a8f834
dstark88 <starkwalker88@gmail.com>
2019-03-09 11:22:30 -0700

    adding SaneP and 911 pages and routes in wizard

---
+++ client/src/pages/Call.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { Col, Row, Container } from "../components/Grid";
+import Jumbotron from "../components/Jumbotron";
+
+function Call() {
+  return (
+    <Container fluid>
+      <Row>
+        <Col size="md-12">
+          <Jumbotron>
+            <h1>This is our Call 911 when not in a safe place page</h1>
+          </Jumbotron>
+        </Col>
+      </Row>
+    </Container>
+  );
+}
+
+export default Call;
