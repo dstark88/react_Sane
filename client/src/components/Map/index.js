50410661152c713ea90b259cca93973ace0da0f0
teenarr <tina.chan91@gmail.com>
2019-03-11 18:30:23 -0600

    styling on marker

--- client/src/components/Map/index.js
+++ client/src/components/Map/index.js
@@ -1,7 +1,21 @@
 import React, { Component } from 'react';
 import GoogleMapReact from 'google-map-react';
  
-const AnyReactComponent = ({ text }) => <div>{text}</div>;
+const AnyReactComponent = ({ text }) => (
+  <div style={{
+    color: 'white', 
+    background: 'grey',
+    padding: '15px 10px',
+    display: 'inline-flex',
+    textAlign: 'center',
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: '100%',
+    transform: 'translate(-50%, -50%)'
+  }}>
+    {text}
+  </div>
+);
  
 class SimpleMap extends Component {
  
@@ -12,7 +26,7 @@ class SimpleMap extends Component {
         <GoogleMapReact
           bootstrapURLKeys={{ key: "AIzaSyAoUsjFYl3W9K6s5G6qEhB7et701_uYIk8" }}
           center={this.props.center}
-          zoom= {6} 
+          zoom= {11} 
         >
           <AnyReactComponent
             lat={this.props.lat}
