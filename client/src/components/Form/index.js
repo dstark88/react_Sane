654f54300543ff0aef5ce56019b45f84cfb03983
Ginger KRC <gingerkr@gmail.com>
2019-03-13 20:20:17 -0600

    button consistency

--- client/src/components/Form/index.js
+++ client/src/components/Form/index.js
@@ -21,7 +21,7 @@ export function TextArea(props) {
 
 export function FormBtn(props) {
   return (
-    <button {...props} style={{ float: "right", marginBottom: 10 }} className="btn btn-success">
+    <button {...props} style={{ float: "right", marginBottom: 10 }}>
       {props.children}
     </button>
   );
