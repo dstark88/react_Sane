6ce76cf4952fafa8d5084491b1eeffcd94780186
teenarr <tina.chan91@gmail.com>
2019-03-13 18:52:03 -0600

    map marker edits

--- client/src/components/Map/index.js
+++ client/src/components/Map/index.js
@@ -13,11 +13,11 @@ class SimpleMap extends Component {
     console.log(this.props.sites);
     return (
       // Important! Always set the container height explicitly
-      <div style={{ height: '300px', width: '100%' }}>
+      <div style={{ height: '300px', width: '100%', marginBottom: "15px" }}>
         <GoogleMapReact
           bootstrapURLKeys={{ key: "AIzaSyAoUsjFYl3W9K6s5G6qEhB7et701_uYIk8" }}
           center={this.props.center}
-          zoom= {11} 
+          zoom= {10} 
         >
 
         {this.props.sites.map(sites => (
