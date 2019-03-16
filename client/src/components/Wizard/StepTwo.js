f313fa2f3b410fd1526489c4af2b2a60ed4bbf9f
dstark88 <starkwalker88@gmail.com>
2019-03-16 12:22:22 -0600

    advocate page

--- client/src/components/Wizard/StepTwo.js
+++ client/src/components/Wizard/StepTwo.js
@@ -1,4 +1,4 @@
 import React from 'react';
 import "./wizard.css"
 
-export const StepTwo = () => <h1 className="text-align-center">Is the person needing assistance over 18?</h1>;
+export const StepTwo = () => <h1 className="text-align-center">Would you like to find a SANE Location or Advocate Resources?</h1>;
