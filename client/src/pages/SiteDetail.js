ea5f267f0a5ebe04afd6408761c9bc2f465467dd
teenarr <tina.chan91@gmail.com>
2019-03-16 13:04:18 -0600

    edits to site details

--- client/src/pages/SiteDetail.js
+++ client/src/pages/SiteDetail.js
@@ -3,10 +3,12 @@ import { Link } from "react-router-dom";
 import { Col, Row, Container } from "../components/Grid";
 import Jumbotron from "../components/Jumbotron";
 import API from "../utils/API";
+import SimpleMap from "../components/Map";
 
 class SiteDetails extends Component {
   state = {
-    site: {}
+    site: {},
+    center: {}
   };
   // When this component mounts, grab the book with the _id of this.props.match.params.id
   // e.g. localhost:3000/books/599dcb67f0f16317844583fc
@@ -14,7 +16,11 @@ class SiteDetails extends Component {
     API.findSite(this.props.match.params.id)
       .then(res => {
         console.log(res.data)
-        this.setState({ site: res.data })})
+        siteCenter = {
+          lat: res.data.Latitude,
+          lng: res.data.Longitude
+        }
+        this.setState({ site: res.data, center: siteCenter })})
       .catch(err => console.log(err));
   }
 
