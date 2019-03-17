e02eb5a73d59e6a26f76e125dc0faf882e45b222
dstark88 <starkwalker88@gmail.com>
2019-03-17 07:27:31 -0600

    content on about us

--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -4,60 +4,47 @@ import { Col, Row, Container } from "../components/Grid";
 import Jumbotron from "../components/Jumbotron";
 
 function About() {
-  return (
-    <div class="container">
-    <div class="row">
-        <div class="col l8 s12">
-            <h5 id="mainInfo">Project Bio</h5>
-            <p id="mainText">We are a team of students in the University of Denver's Women's
+    return (
+      <Container fluid>
+        <Row>
+          <Col size="md-12">
+            <Jumbotron>
+              <h1>About Project SANE</h1>
+            </Jumbotron>
+          </Col>
+        </Row>
+        <Row>
+            <Col size="md-1"></Col>
+            <Col size="md-5">
+                <h3>
+                We are a team of graduates from the University of Denver's Women's
                 Full Stack Web Development Bootcamp. We are passionate about coding and advocating. Our goal
-                with
-                this project is to improve our development skills while providing a platform for important
-                information
-                and tools for sexual assault victims and advocates. </p>
-        </div>
-        <div class="col l2 s12">
-            <h5 id="mainInfo">View Our Portfolios:</h5>
-            <ul>
-                <li><a id="mainText" href="https://brookj1.github.io/jbrooksportfolio/">Jamie Brooks</a></li>
-                <li><a id="mainText" href="https://gingerkrc.github.io/updated-portfolio/">Ginger Conly</a></li>
-                <li><a id="mainText" href="https://tinali91.github.io/portfolio/">Tina Li</a></li>
-                <li><a id="mainText" href="https://dstark88.github.io/Portfolio/">Denise Stark</a></li>
-            </ul>
-        </div>
-        <div class="col l2 s12">
-            <h5 id="mainInfo">Connect on LinkedIn:</h5>
-            <ul>
-                <li><a id="mainText" href="https://www.linkedin.com/in/jamie-brooks-00172321/">Jamie Brooks</a></li>
-                <li><a id="mainText" href="https://www.linkedin.com/in/ginger-conly/">Ginger Conly</a></li>
-                <li><a id="mainText" href="https://www.linkedin.com/in/tinacli/">Tina Li</a></li>
-                <li><a id="mainText" href="https://www.linkedin.com/in/denise-stark-a9387516a/">Denise Stark</a></li>
-            </ul>
-        </div>
-    </div>
-</div>
+                with this project is to improve our development skills while providing a platform for important
+                information and tools for sexual assault victims and advocates. 
+                </h3>
+            </Col>
+            <Col size="md-3">
+                <h4><strong>View Our Portfolios:</strong></h4>
+                  <a href="https://dstark88.github.io/portfolio/" target="blank"><h4>Denise Stark</h4></a>
+                  <a href="https://tinali91.github.io/portfolio/" target="blank"><h4>Tina Li</h4></a>
+                  <a href="https://brookj1.github.io/jbrooksportfolio/" target="blank"><h4>Jamie Brooks</h4></a>
+                  <a href="https://gingerkrc.github.io/updated-portfolio/" target="blank"><h4>Ginger Conly</h4></a>
+            </Col>
+            <Col size="md-3">
+                <h4><strong>Connect on LinkedIn:</strong></h4>
+                  <a href="https://www.linkedin.com/in/denise-stark-a9387516a/" target="blank"><h4>Denise Stark</h4></a>
+                  <a href="https://www.linkedin.com/in/tinacli/" target="blank"><h4>Tina Li</h4></a>
+                  <a href="https://www.linkedin.com/in/jamie-brooks-00172321/" target="blank"><h4>Jamie Brooks</h4></a>
+                  <a href="https://www.linkedin.com/in/ginger-conly/" target="blank"><h4>Ginger Conly</h4></a>
+            </Col>
+        </Row>
+      </Container>
+    );
+  }
+  
+  export default About;
 
-  );
-}
-
-export default About;
-
-// <Container fluid>
-// <Row>
-//   <Col size="md-12">
-//     <Jumbotron>
-//       <h1>This is our about page</h1>
-//     </Jumbotron>
-//   </Col>
-// </Row>
-// <section id="about" class="about-section text center">
-//   <div class="container">
-//     <div class="row">
-//       <div class="col-lg-8 mx-auto">
-//         <p id="mainInfo"> We're a group of women learning to code through University of Denver's Women's Full Stack Coding Bootcamp. We're passionate about learning, coding, and advocating. </p>
-//         <p id="mainInfo">Jamie needs to add links and things here.</p>
-//       </div>
-//     </div>
-//   </div>
-// </section>
-// </Container>
+//                 <li><a id="mainText" href="https://www.linkedin.com/in/jamie-brooks-00172321/">Jamie Brooks</a></li>
+//                 <li><a id="mainText" href="https://www.linkedin.com/in/ginger-conly/">Ginger Conly</a></li>
+//                 <li><a id="mainText" href="https://www.linkedin.com/in/tinacli/">Tina Li</a></li>
+//                 <li><a id="mainText" href="https://www.linkedin.com/in/denise-stark-a9387516a/">Denise Stark</a></li>
